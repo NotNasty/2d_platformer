5e0e1c31ba275dcc4db8961e25744199c616e824
Kononchuk Anastasiia <notnastycode@gmail.com>
2023-07-25 15:01:41 +0300

    08 Fix bug when 1 coin counts 2 times

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -6,7 +6,7 @@ public class Coin : MonoBehaviour
 {
 	private void OnTriggerEnter2D(Collider2D collision)
 	{
-		if (collision.CompareTag("Player"))
+		if (collision.CompareTag("Player") && !collision.isTrigger)
         {
             collision.gameObject.GetComponent<Player>().coins++;
 			Destroy(gameObject);
